93379e11709f8e3cf3ad23d8a04188c386e2add7
sehenry <sarahelhe@gmail.com>
2017-12-13 12:25:54 -0600

    updated js, innerHTML

--- dicegame.js
+++ dicegame.js
@@ -1,31 +1,77 @@
 "use strict"
-var numberOfPlayers= prompt("How many players?");
-var playerName= prompt("Enter your name");
+var playerOneName= prompt("Player 1: Enter your name");
+document.getElementById("player1").innerHTML=playerOneName;
+var playerTwoName= prompt("Player 2:Enter your name");
+document.getElementById("player2").innerHTML=playerTwoName;
 
 
-function rollDice(){
-	var die4 = document.getElementById("d4");
-	var die6 = document.getElementById("d6");
-	var die8 = document.getElementById("d8");
-	var die10 = document.getElementById("d10");
-	var die12 = document.getElementById("d12");
-	var die20 = document.getElementById("d20");
-	var status = document.getElementById("status");
-	var d4=Math.floor(Math.random()* 4) + 1;
-	var d6=Math.floor(Math.random()* 6) + 1;
-	var d8=Math.floor(Math.random()* 8) + 1;
-	var d10=Math.floor(Math.random()* 10) + 1;
-	var d12=Math.floor(Math.random()* 12) + 1;
-	var d20=Math.floor(Math.random()* 20) + 1;
-	var dieResult = ("d4","d6","d8","d10","d12","d20");
-	die4.innerhtml = d4;
-	die6.innerhtml = d6;
-	die8.innerhtml = d8;
-	die10.innerhtml = d10;
-	die12.innerhtml = d12;
-	die20.innerhtml = d20;
-	status.innerhtml="You rolled '+dieResult+'";
-
+function rollDice4(){
+	var die4; 
+	die4=Math.floor(Math.random()* 4) + 1;
+	document.getElementById("result4").innerHTML=die4;
+	//alert(die4);
+if(die4===4){
+	document.getElementById("freeRoll4").innerHTML="That's the highest possible roll! Roll again for free!";
 }
+}
+function rollDice6(){
+	var die6;
+	die6=Math.floor(Math.random()* 6) + 1;
+	document.getElementById("result6").innerHTML=die6;
+
+	
+if(die6===6){
+	document.getElementById("freeRoll6").innerHTML="That's the highest possible roll! Roll again for free!";
+}
+}
+function rollDice8(){
+	var die8; 
+	die8=Math.floor(Math.random()* 8) + 1;
+	document.getElementById("result8").innerHTML=die8;
+if(die8===8){
+	document.getElementById("freeRoll8").innerHTML="That's the highest possible roll! Roll again for free!";
+}
+}
+function rollDice10(){
+	var die10;
+	die10=Math.floor(Math.random()* 10) + 1;
+	document.getElementById("result10").innerHTML=die10;
+if(die10===10){
+	document.getElementById("freeRoll10").innerHTML="That's the highest possible roll! Roll again for free!";
+}
+}
+function rollDice12(){
+	var die12; 
+	die12=Math.floor(Math.random()* 12) + 1;
+	document.getElementById("result12").innerHTML=die12;
+if(die12===12){
+	document.getElementById("freeRoll12").innerHTML="That's the highest possible roll! Roll again for free!";
+}
+}
+function rollDice20(){
+	var die20;
+	die20=Math.floor(Math.random()* 20) + 1;
+	document.getElementById("result20").innerHTML=die20;
+if(die20===20){
+	document.getElementById("freeRoll20").innerHTML="That's the highest possible roll! Roll again for free!";
+}
+}
+
+//var startingMoney
+//var remainingMoney
+//var costOfDice
+//var currentPoints
+//var pointsWon
+//var totalPoints
+
+let startingMoney=100;
+document.getElementById("bank").innerHTML="$"+startingMoney;
+document.getElementById("bank1").innerHTML="$"+startingMoney;
+
+
+
+	
+
+
 
 
